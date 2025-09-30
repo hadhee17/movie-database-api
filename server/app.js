b9b4a82bc82d63c4ffa6e0666144b0a756b95291
hadhee17 <hadhee58@gmail.com>
2025-09-30 10:01:04 +0530

    changes

--- server/app.js
+++ server/app.js
@@ -28,7 +28,7 @@ app.use('/api/v1/movies', movieRoute);
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/review', reviewRoute);
 
-app.get('/', (req, res) => {
+app.use('/', (req, res) => {
   res.json({ message: 'Hello from movie database api' });
 });
 
