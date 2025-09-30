f7c0a9046eef43d0b70ef7766a003eccddb66a04
hadhee17 <hadhee58@gmail.com>
2025-09-30 07:20:01 +0530

    cors updates

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.set('query parser', (str) => qs.parse(str));
 app.use(express.json());
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://movie-database-api-zpam.vercel.app', // local Vite dev server
+  'https://movie-database-api-zpam.vercel.app/', // local Vite dev server
   // deployed frontend
 ];
 app.use(
